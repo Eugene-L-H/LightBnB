64e546f85cc8377b545b6287273928fa1cfdbe72
Eugene Helland <eugenehelland@mail.com>
2022-04-08 09:35:06 -0600

    Refactor database functions pull data from database and to insert data as well.

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -21,16 +21,23 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  queryString = `
+    SELECT *
+    FROM users
+    WHERE users.email = $1;
+  `;
+
+  return pool
+    .query(queryString, [email])
+    .then((result) => {
+      if (result.rows.length === 1) {
+        return result.rows[0];
+      }
+      return null;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    })
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -40,7 +47,25 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  queryString = `
+  SELECT *
+  FROM users
+  WEHRE users.id = $1;
+`;
+
+  return pool
+    .query(queryString, [id])
+    .then((result) => {
+      console.log(result.rows);
+      if (result.rows.length === 1) {
+        console.log(result.rows);
+        return result.rows[0];
+      }
+      return null;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    })
 }
 exports.getUserWithId = getUserWithId;
 
@@ -51,10 +76,20 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  queryString = `
+    INSERT into users (name, email, password)
+    VALUES ($1, $2, $3);
+  `;
+
+  return pool
+    .query(queryString, [user.name, user.email, user.password])
+    .then((result) => {
+      if (result) return result.rows[0];
+      return null;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    })
 }
 exports.addUser = addUser;
 
